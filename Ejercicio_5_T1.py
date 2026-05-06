46adfae90f0d52b5c521bdaa9c8fdf563613a0a4
ederhoppe <eder.creaper@gmail.com>
2026-05-06 14:25:03 -0600

    feat: implementar ejercicio 5 (suma de enteros) y documentacion
    Se añade script para la suma de dos números enteros. Se incluye manejo de casting con int() y docstring con el enunciado oficial del ejercicio

---
+++ Ejercicio_5_T1.py
@@ -0,0 +1,8 @@
+"""5. Escribe un programa que te permita ingresar dos numeros enteros y 
+como salida de el resultado de la suma"""
+
+print('Bienvenido a la calculadora de numeros enteros')
+x = int(input('Ingresa un numero: '))
+y = int(input('Ingresa un segundo numero: '))
+z = x + y
+print('El resultado es: ', z)
